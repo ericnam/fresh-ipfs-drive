77c6ef93a41829f40d7ecd0f8fc5f3fe240b47eb
ericnam <ericnam860@gmail.com>
2019-07-22 23:51:40 -0500

    Get entire directory from pi node function

--- src/context/index.js
+++ src/context/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import createUseContext from "constate"; 
 import { FILE_TYPE, FOLDER_TYPE, HOME_NODE_ADDRS } from '@Utils/constants';
+import { findDir, pullDirectory } from '@Utils/ipfs';
 import multiaddr from 'multiaddr';
 
 const ipfs = require('ipfs');
@@ -30,48 +31,6 @@ const root = {
 
 const testHash = "QmRWZeYxzfTm9LwUCQcj3QVLMZgzRFVqjiYp8aeyconrmh";
 
-const searchDir = (dir, entryPath, i) => {
-  var pathArray = entryPath.split('/');
-  if (pathArray.length - 1 === i) {
-    return dir;
-  }  
-  return searchDir(dir.children.find(x => x.name === pathArray[i]), entryPath, i+1);
-}
-
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}
-
-const pullDirectory = async (ipfs, hash) => {
-  var directory = [];
-
-  ipfs.ls(hash, async (err, files) => {
-    if (err) { console.log(err); }
-
-    asyncForEach(files, async (file) => {
-      directory.push(file);
-      if (file.type === 'dir') {
-        const subDir = await pullDirectory(ipfs, file.path);
-        directory.concat(subDir);
-      }
-    })
-
-    // files.forEach((file) => {
-    //   if (err) { console.log(err); }
-    //   directory.push(file);
-    //   if (file.type === 'dir') {
-    //     const subDir = await pullDirectory(ipfs, file.path);
-    //     directory.concat(subDir);
-    //   }
-    // });
-
-    // console.log(directory)
-    return directory;
-  });
-}
-
 const useDirectory = () => {
 
   // app states
@@ -82,31 +41,16 @@ const useDirectory = () => {
   ipfs_node.on('ready', () => {    
     // Connect to raspi node
     ipfs_node.swarm.connect(HOME_NODE_ADDRS, async (err) => {
-      if (err) { 
-        console.log(err); 
-      }
-      else { 
-        console.log('Successfully connected to rasp pi node.'); 
-      }
-
+      console.log(err ? err : 'Successfully connected to rasp pi node.'); 
       const rootDir = await pullDirectory(ipfs_node, testHash);
       console.log(rootDir);
     });
-
-
-    // ipfs_node.ls(testHash, (err, files) => {
-    //   console.log('ls hit');
-    //   if (err) { console.log(err); }
-    //   files.forEach((file) => {
-    //     console.log(file.path + " " + file.type)
-    //   })
-    // });
   });
 
   const AddFile = (newEntry) => {
     updateDir(prevDirStr => { 
       var prevDir = JSON.parse(prevDirStr);
-      searchDir(prevDir, newEntry.path, 1).children.push(newEntry);
+      findDir(prevDir, newEntry.path, 1).children.push(newEntry);
       return JSON.stringify(prevDir);
     });
   };
--- src/utils/ipfs.js
+++ src/utils/ipfs.js
@@ -1,3 +1,30 @@
-const getIPFSDirectory = (ipfs) => {
-  
+import { asyncForEach } from '@Utils/helpers'
+
+const ipfs_ls = async (ipfs, addr) => {
+  return ipfs.ls(addr);
 }
+
+const findDir = (dir, entryPath, i) => {
+  var pathArray = entryPath.split('/');
+  if (pathArray.length - 1 === i) {
+    return dir;
+  }  
+  return findDir(dir.children.find(x => x.name === pathArray[i]), entryPath, i+1);
+}
+
+const pullDirectory = async (ipfs, hash) => {
+  var directory = new Array();
+  await ipfs_ls(ipfs, hash).then(async (files) => {
+    await asyncForEach(files, async (file) => {
+        directory.push(file);  
+        if (file.type === 'dir') {
+          const subDir = await pullDirectory(ipfs, file.path);
+          directory = directory.concat(subDir);
+        };
+    });
+  });
+
+  return directory;
+}
+
+export { ipfs_ls, findDir, pullDirectory };
